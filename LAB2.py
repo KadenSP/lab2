e4b78b3b31938dca05e2eed979bd75d8f0cf026d
Kaden <KADEN.23@ichat.sp.edu.sg>
2024-04-27 18:51:06 +0800

    median

--- LAB2.py
+++ LAB2.py
@@ -1,9 +1,10 @@
-
+import statistics
 def main():
     display_main_menu()
     UI=get_user_input()
     calc_average(UI)
     find_min_max(UI)
+    calc_median_temperature(UI)
 
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
@@ -24,6 +25,11 @@ def find_min_max(UI):
     max_v = max(UI)
     print("Minimum value:", min_v)
     print("Maximum value:", max_v)
+
+def calc_median_temperature(UI):
+    sorted_input=sorted(UI)
+    median=statistics.median(sorted_input)
+    print("median =", median)
 
 main()
 
